0d2724362d2139a039f3beba31d47fec864a309d
Shi <mo@shi.moe>
2020-06-08 18:25:21 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ function updateCollection() {
       </div>
     `
   });
+  
+  for (let [key, value] in Object.entries(collection)) {
+    insertParam(key, value);
+  }
 };
 
 function addItem(target) {
@@ -36,6 +40,33 @@ function addItem(target) {
   let item = items.find((item) => itemID == item.id);
   collection.push(item);
   updateCollection();
+  console.log(collection);
 }
 
-console.log(collection);
+function insertParam(key, value) {
+  key = encodeURIComponent(key);
+  value = encodeURIComponent(value);
+
+  // kvp looks like ['key1=value1', 'key2=value2', ...]
+  var kvp = document.location.search.substr(1).split('&');
+  let i=0;
+
+  for(; i<kvp.length; i++) {
+    if (kvp[i].startsWith(key + '=')) {
+      let pair = kvp[i].split('=');
+      pair[1] = value;
+      kvp[i] = pair.join('=');
+      break;
+    }
+  }
+
+  if(i >= kvp.length) {
+    kvp[kvp.length] = [key,value].join('=');
+  }
+
+  // can return this or...
+  let params = kvp.join('&');
+
+  // reload page with new params
+  document.location.search = params;
+}
