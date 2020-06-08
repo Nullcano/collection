395d1c3922cd6ce6fb4e64aea437813d977f4a6e
Shi <mo@shi.moe>
2020-06-08 16:53:33 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    id: 1,
+    name: `Item One`,
+    desc: `Item One Description.`,
+    image: `https://purr.objects-us-east-1.dream.io/i/EJmsm.jpg`
+  }, {
+    id: 2,
+    name: `Item Two`,
+    desc: `Item Two Description.`,
+    image: `https://purr.objects-us-east-1.dream.io/i/EFtYj.jpg`
+  }, {
+    id: 3,
+    name: `Item Three`,
+    desc: `Item Three Description.`,
+    image: `https://purr.objects-us-east-1.dream.io/i/img_20160924_170252.jpg`
+  }
+]
